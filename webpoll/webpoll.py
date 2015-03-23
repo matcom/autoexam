010c3437e2f4533f74ae74d6f567b6daa924157e
Alejandro Piad <alepiad@gmail.com>
2015-03-23 01:32:54 -0400

    Added an option to vote for all in polls

--- webpoll/webpoll.py
+++ webpoll/webpoll.py
@@ -17,13 +17,18 @@ def data():
 	people = []
 	data = None
 
+	all_sum = 0
+
 	if os.path.exists(app.data_file):
 		with open(app.data_file) as fp:
 			data = json.load(fp)
 
+			if app.all is not None:
+				all_sum = data['1']['options'][str(app.all)]
+
 	for i,n in enumerate(app.names):
 		if data is not None:
-			people.append(dict(name=n, votes=data['1']['options'][str(i)]))
+			people.append(dict(name=n, votes=data['1']['options'][str(i)] + all_sum))
 		else:
 			people.append(dict(name=n, votes=0))
 
@@ -32,5 +37,6 @@ def data():
 
 def run(args):
 	app.data_file = os.path.abspath(args.data)
+	app.all = args.all
 	app.names = [s.strip() for s in open(args.names).readlines() if s.strip()]
 	app.run(host=args.host, port=args.port, debug=args.debug)
