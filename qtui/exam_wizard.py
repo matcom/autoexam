b07c26c4d00de2b7dd184e0d173ec9e03ce4b456
Alex Coto <alexrc09@gmail.com>
2015-08-27 19:15:41 -0400

    Comment out temporally scoresheet editing page

--- qtui/exam_wizard.py
+++ qtui/exam_wizard.py
@@ -13,8 +13,9 @@ class ExamWizard(QWizard):
         self.project = project
         self.order = None # TODO: Implement order loading here?
         self.results = None # TODO: Implement result loading here?
+        self.setOption(QWizard.IndependentPages, False)
         self.addPage(MasterPage(project, self))
         self.addPage(GeneratePage(project, self))
         self.addPage(ScanPage(project, self))
-        self.addPage(ScoresPage(project, self))
+        # self.addPage(ScoresPage(project, self))
         self.addPage(ResultsPage(project, self))
